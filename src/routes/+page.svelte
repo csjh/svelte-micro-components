d745e04917d8a2bfd621f667bbe9e5cbd08a8dba
csjh <cormick.holland@gmail.com>
2023-02-26 16:10:58 -0500

    added basic test on +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to your library project</h1>
+<script lang="ts">
+    import m from "$lib";
+
+    const Component = m`<h1>Welcome to your library project</h1>`
+</script>
+
+<Component />
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
