c97ea9d2dbc5ec970b830d01dd53c9de568f7b33
csjh <cormick.holland@gmail.com>
2023-02-27 02:32:56 -0500

    code cleanup

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -12,10 +12,14 @@ import {
 } from 'svelte/internal';
 import { BROWSER } from 'esm-env';
 
+// unholy type declaration, should fix
+type Microcomponent<T extends string> = typeof SvelteComponentTyped<Record<T, string>> &
+	SvelteComponentTyped<Record<T, string>> & { $$values: Record<T, Text>; $$nodes: Node[] };
+
 export default function micro_component<T extends string>(
 	strings: TemplateStringsArray,
 	...propNames: T[]
-): typeof SvelteComponentTyped<Record<T, string>> {
+): Microcomponent<T> {
 	const convert = (fn: (propName: T) => string) =>
 		strings.map((s, i) => s + (propNames[i] ? fn(propNames[i]) : '')).join('');
 
@@ -29,47 +33,44 @@ export default function micro_component<T extends string>(
 	template.innerHTML = convert((propName) => `<template-${propName} />`);
 	const node = template.content;
 
-	function initialize(
-		cmt: SvelteComponentTyped<Record<T, string>> & { $$values: Record<T, Text>; $$nodes: Node[] },
-		props: Record<T, string>
-	) {
-		cmt.$$ = {
+	function initialize(component: Microcomponent<T>, props: Record<T, string>) {
+		component.$$ = {
 			on_mount: [],
 			after_update: [],
 			// @ts-expect-error other fields shouldn't matter
 			fragment: {
 				c: () => {
-					cmt.$$nodes = Array.from((node.cloneNode(true) as HTMLElement).children).reverse();
+					component.$$nodes = Array.from((node.cloneNode(true) as HTMLElement).children);
 					for (const propName of propNames) {
-						cmt.$$values[propName] = text(props[propName]);
+						component.$$values[propName] = text(props[propName]);
 					}
 				},
 				m: (target, anchor) => {
 					// @ts-expect-error we know c is defined
 					// TODO: also side note no clue why this is needed should look into it
-					if (!cmt.$$template) cmt.$$.fragment.c();
+					if (!component.$$template) component.$$.fragment.c();
 
 					for (const propName of propNames) {
-						cmt.$$nodes[0]!.parentNode!.querySelector(`template-${propName}`)!.replaceWith(
-							cmt.$$values[propName]
+						component.$$nodes[0]!.parentNode!.querySelector(`template-${propName}`)!.replaceWith(
+							component.$$values[propName]
 						);
 					}
 
-					cmt.$$nodes.forEach((node) => insert(target, node, anchor));
+					component.$$nodes.forEach((node) => insert(target, node, anchor));
 				},
 				l: noop,
 				p: noop,
 				i: noop,
 				o: noop,
 				d: (detaching) => {
-					if (detaching) cmt.$$nodes.forEach(detach);
+					if (detaching) component.$$nodes.forEach(detach);
 				}
 			}
 		};
 
-		cmt.$$set = (props) => {
+		component.$$set = (props) => {
 			for (const propName of propNames) {
-				cmt.$$values[propName].data = props[propName];
+				component.$$values[propName].data = props[propName];
 			}
 		};
 	}
