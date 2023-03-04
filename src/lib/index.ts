a23c1442eb17c3a21d299729454d9286814e9719
csjh <cormick.holland@gmail.com>
2023-03-03 20:44:52 -0500

    formatted

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -11,34 +11,54 @@ import {
 	element,
 	blank_object,
 	children,
-    attr,
-    bubble,
-    listen,
-    run_all
+	attr,
+	bubble,
+	listen,
+	run_all
 } from 'svelte/internal';
-import type { Events, Directive, OnDirective, UseDirective, Action, Prop, Microcomponent } from "./types";
+import type {
+	Events,
+	Directive,
+	OnDirective,
+	UseDirective,
+	Action,
+	Prop,
+	Microcomponent
+} from './types';
 import { BROWSER } from 'esm-env';
 
 const isDirective = (value: unknown): value is Directive => Array.isArray(value);
 
 // on:eventname
 // on`${eventName}` or on`${eventname}=${}`
-export function on<T extends keyof Events>(_: TemplateStringsArray, eventName: T, alias?: string): OnDirective<T> {
-    return ["on", eventName, alias ?? eventName];
+export function on<T extends keyof Events>(
+	_: TemplateStringsArray,
+	eventName: T,
+	alias?: string
+): OnDirective<T> {
+	return ['on', eventName, alias ?? eventName];
 }
 
 // use:action={params}
 // use`${action}=${params}`
-export function use<Element = HTMLElement, Parameter = any, Attributes extends Record<string, any> = Record<never, any>>(_: TemplateStringsArray, action: Action<Element, Parameter, Attributes>, params?: Parameter): UseDirective {
-    return ["use", action, params];
+export function use<
+	Element = HTMLElement,
+	Parameter = any,
+	Attributes extends Record<string, any> = Record<never, any>
+>(
+	_: TemplateStringsArray,
+	action: Action<Element, Parameter, Attributes>,
+	params?: Parameter
+): UseDirective {
+	return ['use', action, params];
 }
 
 export default function micro_component<Props extends readonly Prop[]>(
 	{ raw: strings }: TemplateStringsArray,
 	...propNames: Props
 ): Microcomponent<Props[number]> {
-    type T = Prop;
-    type StringProps = Extract<T, string>;
+	type T = Prop;
+	type StringProps = Extract<T, string>;
 
 	const convert = (fn: (propName: T, previousString: string) => string) =>
 		strings.map((s, i) => (propNames[i] ? fn(propNames[i], s) : s)).join('');
@@ -46,39 +66,41 @@ export default function micro_component<Props extends readonly Prop[]>(
 	if (!BROWSER) {
 		return create_ssr_component((_: unknown, $$props: Record<StringProps, string>) => {
 			return convert((propName, previousString) =>
-                isDirective(propName)? previousString :
-				previousString.at(-1) === '='
+				isDirective(propName)
+					? previousString
+					: previousString.at(-1) === '='
 					? previousString.slice(0, previousString.lastIndexOf(' ') + 1)
 					: previousString + escape($$props[propName])
 			);
 		}) as any;
 	}
 
-	const categorized: { attr: Set<StringProps>; text: Set<StringProps>; events: Set<OnDirective> } = { attr: new Set(), text: new Set(), events: new Set() };
+	const categorized: { attr: Set<StringProps>; text: Set<StringProps>; events: Set<OnDirective> } =
+		{ attr: new Set(), text: new Set(), events: new Set() };
 	propNames.forEach((propName, i) => {
-        if (isDirective(propName)) {
-            if (propName[0] === 'on') {
-                categorized.events.add(propName);
-            }
-        } else {
-            if (strings[i].at(-1) === '=') {
-                categorized.attr.add(propName);
-            } else {
-                categorized.text.add(propName);
-            }
-        }
+		if (isDirective(propName)) {
+			if (propName[0] === 'on') {
+				categorized.events.add(propName);
+			}
+		} else {
+			if (strings[i].at(-1) === '=') {
+				categorized.attr.add(propName);
+			} else {
+				categorized.text.add(propName);
+			}
+		}
 	});
 
 	const classes = {} as Record<StringProps, string>;
 	function classify(propName: T, previousString: string) {
-        if (isDirective(propName)) {
-            return previousString + ` data-${propName[1]}-${propName[2]} `;
-        } else {
-            if (previousString.at(-1) !== '=') return previousString + `<template-${propName} />`;
-            const start = previousString.lastIndexOf(' ');
-            classes[propName] = previousString.slice(start + 1, -1);
-            return previousString.slice(0, start) + ` data-${propName} `;
-        }
+		if (isDirective(propName)) {
+			return previousString + ` data-${propName[1]}-${propName[2]} `;
+		} else {
+			if (previousString.at(-1) !== '=') return previousString + `<template-${propName} />`;
+			const start = previousString.lastIndexOf(' ');
+			classes[propName] = previousString.slice(start + 1, -1);
+			return previousString.slice(0, start) + ` data-${propName} `;
+		}
 	}
 
 	const template = element('template');
@@ -89,8 +111,8 @@ export default function micro_component<Props extends readonly Prop[]>(
 		const values: Record<StringProps, Attr | Text> = blank_object();
 
 		let nodes: ChildNode[];
-        let dispose: (() => void)[] = [];
-        let mounted = false;
+		let dispose: (() => void)[] = [];
+		let mounted = false;
 
 		component.$$ = {
 			on_mount: [],
@@ -114,7 +136,7 @@ export default function micro_component<Props extends readonly Prop[]>(
 				m: (target, anchor) => {
 					// @ts-expect-error we know c is defined
 					// TODO: also side note no clue why this is needed should look into it
-                    // maybe because of hydration?
+					// maybe because of hydration?
 					if (!component.$$template) component.$$.fragment.c();
 
 					const parent = nodes[0]!.parentNode!;
@@ -126,15 +148,17 @@ export default function micro_component<Props extends readonly Prop[]>(
 						attr(el, `data-${propName}`);
 						el.setAttributeNode(values[propName] as Attr);
 					}
-                    if (!mounted) {
-                        for (const event of categorized.events) {
-                            const el = parent.querySelector(`[data-${event[1]}-${event[2]}]`)!;
-                            dispose.push(listen(el, event[1], function(this: HTMLElement, e: Event) { 
-                                bubble.call(this, component, e);
-                            }));
-                        }
-                        mounted = true;
-                    }
+					if (!mounted) {
+						for (const event of categorized.events) {
+							const el = parent.querySelector(`[data-${event[1]}-${event[2]}]`)!;
+							dispose.push(
+								listen(el, event[1], function (this: HTMLElement, e: Event) {
+									bubble.call(this, component, e);
+								})
+							);
+						}
+						mounted = true;
+					}
 
 					// note: can insert all with Template.content, but then can't access nodes after insertion
 					nodes.forEach((node) => insert(target, node, anchor));
@@ -145,8 +169,8 @@ export default function micro_component<Props extends readonly Prop[]>(
 				o: noop,
 				d: (detaching) => {
 					if (detaching) nodes.forEach(detach);
-                    mounted = false;
-                    run_all(dispose);
+					mounted = false;
+					run_all(dispose);
 				}
 			}
 		};
