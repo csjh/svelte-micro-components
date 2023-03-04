ab83fa4477ea97bcba07c70d2a8db9bd19971238
csjh <cormick.holland@gmail.com>
2023-03-03 20:42:27 -0500

    added event bubbling/alias

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -11,24 +11,37 @@ import {
 	element,
 	blank_object,
 	children,
-    attr
+    attr,
+    bubble,
+    listen,
+    run_all
 } from 'svelte/internal';
+import type { Events, Directive, OnDirective, UseDirective, Action, Prop, Microcomponent } from "./types";
 import { BROWSER } from 'esm-env';
 
-// unholy type declaration, should fix
-type Microcomponent<T extends string> = typeof SvelteComponentTyped<Record<T, string>> &
-	SvelteComponentTyped<Record<T, string>>;
+const isDirective = (value: unknown): value is Directive => Array.isArray(value);
+
+// on:eventname
+// on`${eventName}` or on`${eventname}=${}`
+export function on<T extends keyof Events>(_: TemplateStringsArray, eventName: T, alias?: string): OnDirective<T> {
+    return ["on", eventName, alias ?? eventName];
+}
 
 export default function micro_component<T extends string>(
+export default function micro_component<Props extends readonly Prop[]>(
 	{ raw: strings }: TemplateStringsArray,
-	...propNames: T[]
-): Microcomponent<T> {
+	...propNames: Props
+): Microcomponent<Props[number]> {
+    type T = Prop;
+    type StringProps = Extract<T, string>;
+
 	const convert = (fn: (propName: T, previousString: string) => string) =>
 		strings.map((s, i) => (propNames[i] ? fn(propNames[i], s) : s)).join('');
 
 	if (!BROWSER) {
-		return create_ssr_component((_: unknown, $$props: Record<T, string>) => {
+		return create_ssr_component((_: unknown, $$props: Record<StringProps, string>) => {
 			return convert((propName, previousString) =>
+                isDirective(propName)? previousString :
 				previousString.at(-1) === '='
 					? previousString.slice(0, previousString.lastIndexOf(' ') + 1)
 					: previousString + escape($$props[propName])
@@ -36,31 +49,43 @@ export default function micro_component<T extends string>(
 		}) as any;
 	}
 
-	const categorized: { attr: Set<T>; text: Set<T> } = { attr: new Set(), text: new Set() };
+	const categorized: { attr: Set<StringProps>; text: Set<StringProps>; events: Set<OnDirective> } = { attr: new Set(), text: new Set(), events: new Set() };
 	propNames.forEach((propName, i) => {
-		if (strings[i].at(-1) === '=') {
-			categorized.attr.add(propName);
-		} else {
-			categorized.text.add(propName);
-		}
+        if (isDirective(propName)) {
+            if (propName[0] === 'on') {
+                categorized.events.add(propName);
+            }
+        } else {
+            if (strings[i].at(-1) === '=') {
+                categorized.attr.add(propName);
+            } else {
+                categorized.text.add(propName);
+            }
+        }
 	});
 
-	const classes = {} as Record<T, string>;
+	const classes = {} as Record<StringProps, string>;
 	function classify(propName: T, previousString: string) {
-		if (previousString.at(-1) !== '=') return previousString + `<template-${propName} />`;
-		const start = previousString.lastIndexOf(' ');
-		classes[propName] = previousString.slice(start + 1, -1);
-		return previousString.slice(0, start) + ` data-${propName} `;
+        if (isDirective(propName)) {
+            return previousString + ` data-${propName[1]}-${propName[2]} `;
+        } else {
+            if (previousString.at(-1) !== '=') return previousString + `<template-${propName} />`;
+            const start = previousString.lastIndexOf(' ');
+            classes[propName] = previousString.slice(start + 1, -1);
+            return previousString.slice(0, start) + ` data-${propName} `;
+        }
 	}
 
 	const template = element('template');
 	template.innerHTML = convert(classify);
 	const node = template.content;
 
-	function initialize(component: Microcomponent<T>, props: Record<T, string>) {
-		const values: Record<T, Attr | Text> = blank_object();
+	function initialize(component: Microcomponent<T>, props: Record<StringProps, string>) {
+		const values: Record<StringProps, Attr | Text> = blank_object();
 
 		let nodes: ChildNode[];
+        let dispose: (() => void)[] = [];
+        let mounted = false;
 
 		component.$$ = {
 			on_mount: [],
@@ -84,6 +109,7 @@ export default function micro_component<T extends string>(
 				m: (target, anchor) => {
 					// @ts-expect-error we know c is defined
 					// TODO: also side note no clue why this is needed should look into it
+                    // maybe because of hydration?
 					if (!component.$$template) component.$$.fragment.c();
 
 					const parent = nodes[0]!.parentNode!;
@@ -95,6 +121,15 @@ export default function micro_component<T extends string>(
 						attr(el, `data-${propName}`);
 						el.setAttributeNode(values[propName] as Attr);
 					}
+                    if (!mounted) {
+                        for (const event of categorized.events) {
+                            const el = parent.querySelector(`[data-${event[1]}-${event[2]}]`)!;
+                            dispose.push(listen(el, event[1], function(this: HTMLElement, e: Event) { 
+                                bubble.call(this, component, e);
+                            }));
+                        }
+                        mounted = true;
+                    }
 
 					// note: can insert all with Template.content, but then can't access nodes after insertion
 					nodes.forEach((node) => insert(target, node, anchor));
@@ -105,11 +140,13 @@ export default function micro_component<T extends string>(
 				o: noop,
 				d: (detaching) => {
 					if (detaching) nodes.forEach(detach);
+                    mounted = false;
+                    run_all(dispose);
 				}
 			}
 		};
 
-		component.$$set = (props: Record<T, string>) => {
+		component.$$set = (props: Record<StringProps, string>) => {
 			for (const prop in props) {
 				values[prop].nodeValue = props[prop];
 			}
---
+++ src/lib/types.ts
@@ -0,0 +1,162 @@
+import type { AnimationEventHandler, ClipboardEventHandler, CompositionEventHandler, DragEventHandler, EventHandler, FocusEventHandler, FormEventHandler, KeyboardEventHandler, MessageEventHandler, MouseEventHandler, PointerEventHandler, TouchEventHandler, TransitionEventHandler, UIEventHandler, WheelEventHandler } from "svelte/elements";
+import type { Action } from 'svelte/action';
+import type { SvelteComponentTyped } from "svelte";
+
+export { Action };
+export type OnDirective<T extends keyof Events = keyof Events> = ["on", T, string];
+export type UseDirective = ["use", Action, unknown?];
+
+export type Prop = string | Directive;
+
+// unholy type declaration, should fix
+export type Microcomponent<T extends Prop> = 
+    typeof SvelteComponentTyped<
+        Extract<T, string> extends never? never : Record<Extract<T, string>, string>,
+        { [K in Extract<T, OnDirective>[1]]: Parameters<Events[K]> }
+    > &
+	SvelteComponentTyped<
+    Extract<T, string> extends never? never : Record<Extract<T, string>, string>,
+        { [K in Extract<T, OnDirective>[1]]: Parameters<Events[K]> }
+    >;
+
+export interface Events {
+	// Clipboard Events
+	'copy': ClipboardEventHandler<HTMLElement>;
+	'cut': ClipboardEventHandler<HTMLElement>;
+	'paste': ClipboardEventHandler<HTMLElement>;
+
+	// Composition Events
+	'compositionend': CompositionEventHandler<HTMLElement>;
+	'compositionstart': CompositionEventHandler<HTMLElement>;
+	'compositionupdate': CompositionEventHandler<HTMLElement>;
+
+	// Focus Events
+	'focus': FocusEventHandler<HTMLElement>;
+	'focusin': FocusEventHandler<HTMLElement>;
+	'focusout': FocusEventHandler<HTMLElement>;
+	'blur': FocusEventHandler<HTMLElement>;
+
+	// Form Events
+	'change': FormEventHandler<HTMLElement>;
+	'beforeinput': EventHandler<InputEvent, HTMLElement>;
+	'input': FormEventHandler<HTMLElement>;
+	'reset': FormEventHandler<HTMLElement>;
+	'submit': EventHandler<Event & { readonly submitter: HTMLElement | null; }, HTMLElement>; // TODO make this SubmitEvent once we require TS>=4.4
+	'invalid': EventHandler<Event, HTMLElement>;
+	'formdata': EventHandler<Event & { readonly formData: FormData; }, HTMLElement>; // TODO make this FormDataEvent once we require TS>=4.4
+
+	// Image Events
+	'load': EventHandler;
+	'error': EventHandler; // also a Media Event
+
+	// Detail Events
+	'toggle': EventHandler<Event, HTMLElement>;
+
+	// Keyboard Events
+	'keydown': KeyboardEventHandler<HTMLElement>;
+	'keypress': KeyboardEventHandler<HTMLElement>;
+	'keyup': KeyboardEventHandler<HTMLElement>;
+
+	// Media Events
+	'abort': EventHandler<Event, HTMLElement>;
+	'canplay': EventHandler<Event, HTMLElement>;
+	'canplaythrough': EventHandler<Event, HTMLElement>;
+	'cuechange': EventHandler<Event, HTMLElement>;
+	'durationchange': EventHandler<Event, HTMLElement>;
+	'emptied': EventHandler<Event, HTMLElement>;
+	'encrypted': EventHandler<Event, HTMLElement>;
+	'ended': EventHandler<Event, HTMLElement>;
+	'loadeddata': EventHandler<Event, HTMLElement>;
+	'loadedmetadata': EventHandler<Event, HTMLElement>;
+	'loadstart': EventHandler<Event, HTMLElement>;
+	'pause': EventHandler<Event, HTMLElement>;
+	'play': EventHandler<Event, HTMLElement>;
+	'playing': EventHandler<Event, HTMLElement>;
+	'progress': EventHandler<Event, HTMLElement>;
+	'ratechange': EventHandler<Event, HTMLElement>;
+	'seeked': EventHandler<Event, HTMLElement>;
+	'seeking': EventHandler<Event, HTMLElement>;
+	'stalled': EventHandler<Event, HTMLElement>;
+	'suspend': EventHandler<Event, HTMLElement>;
+	'timeupdate': EventHandler<Event, HTMLElement>;
+	'volumechange': EventHandler<Event, HTMLElement>;
+	'waiting': EventHandler<Event, HTMLElement>;
+
+	// MouseEvents
+	'auxclick': MouseEventHandler<HTMLElement>;
+	'click': MouseEventHandler<HTMLElement>;
+	'contextmenu': MouseEventHandler<HTMLElement>;
+	'dblclick': MouseEventHandler<HTMLElement>;
+	'drag': DragEventHandler<HTMLElement>;
+	'dragend': DragEventHandler<HTMLElement>;
+	'dragenter': DragEventHandler<HTMLElement>;
+	'dragexit': DragEventHandler<HTMLElement>;
+	'dragleave': DragEventHandler<HTMLElement>;
+	'dragover': DragEventHandler<HTMLElement>;
+	'dragstart': DragEventHandler<HTMLElement>;
+	'drop': DragEventHandler<HTMLElement>;
+	'mousedown': MouseEventHandler<HTMLElement>;
+	'mouseenter': MouseEventHandler<HTMLElement>;
+	'mouseleave': MouseEventHandler<HTMLElement>;
+	'mousemove': MouseEventHandler<HTMLElement>;
+	'mouseout': MouseEventHandler<HTMLElement>;
+	'mouseover': MouseEventHandler<HTMLElement>;
+	'mouseup': MouseEventHandler<HTMLElement>;
+
+	// Selection Events
+	'select': EventHandler<Event, HTMLElement>;
+	'selectionchange': EventHandler<Event, HTMLElement>;
+	'selectstart': EventHandler<Event, HTMLElement>;
+
+	// Touch Events
+	'touchcancel': TouchEventHandler<HTMLElement>;
+	'touchend': TouchEventHandler<HTMLElement>;
+	'touchmove': TouchEventHandler<HTMLElement>;
+	'touchstart': TouchEventHandler<HTMLElement>;
+
+	// Pointer Events
+	'gotpointercapture': PointerEventHandler<HTMLElement>;
+	'pointercancel': PointerEventHandler<HTMLElement>;
+	'pointerdown': PointerEventHandler<HTMLElement>;
+	'pointerenter': PointerEventHandler<HTMLElement>;
+	'pointerleave': PointerEventHandler<HTMLElement>;
+	'pointermove': PointerEventHandler<HTMLElement>;
+	'pointerout': PointerEventHandler<HTMLElement>;
+	'pointerover': PointerEventHandler<HTMLElement>;
+	'pointerup': PointerEventHandler<HTMLElement>;
+	'lostpointercapture': PointerEventHandler<HTMLElement>;
+
+	// UI Events
+	'scroll': UIEventHandler<HTMLElement>;
+	'resize': UIEventHandler<HTMLElement>;
+
+	// Wheel Events
+	'wheel': WheelEventHandler<HTMLElement>;
+
+	// Animation Events
+	'animationstart': AnimationEventHandler<HTMLElement>;
+	'animationend': AnimationEventHandler<HTMLElement>;
+	'animationiteration': AnimationEventHandler<HTMLElement>;
+
+	// Transition Events
+	'transitionstart': TransitionEventHandler<HTMLElement>;
+	'transitionrun': TransitionEventHandler<HTMLElement>;
+	'transitionend': TransitionEventHandler<HTMLElement>;
+	'transitioncancel': TransitionEventHandler<HTMLElement>;
+
+	// Svelte Transition Events
+	'outrostart': EventHandler<CustomEvent<null>, HTMLElement>;
+	'outroend': EventHandler<CustomEvent<null>, HTMLElement>;
+	'introstart': EventHandler<CustomEvent<null>, HTMLElement>;
+	'introend': EventHandler<CustomEvent<null>, HTMLElement>;
+
+	// Message Events
+	'message': MessageEventHandler<HTMLElement>;
+	'messageerror': MessageEventHandler<HTMLElement>;
+
+	// Global Events
+	'cancel': EventHandler<Event, HTMLElement>;
+	'close': EventHandler<Event, HTMLElement>;
+	'fullscreenchange': EventHandler<Event, HTMLElement>;
+	'fullscreenerror': EventHandler<Event, HTMLElement>;
+}
